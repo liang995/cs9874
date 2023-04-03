e574d43f502fb9d05db42f804fa0d346e01f69c0
calvin <clvn.liang@gmail.com>
2023-04-03 15:26:34 -0400

    current model without gabor

--- model/discriminator.py
+++ model/discriminator.py
@@ -4,7 +4,8 @@ from model.conv_bn_relu import ConvBNRelu
 from model.gabor import Gabor
 from torchvision import transforms
 import torch 
-
+import time
+from model.gabormod import Model
 
 class Discriminator(nn.Module):
     """
@@ -12,8 +13,8 @@ class Discriminator(nn.Module):
     """
     def __init__(self, config: HiDDenConfiguration):
         super(Discriminator, self).__init__()
-
-        layers = [ConvBNRelu(3, config.discriminator_channels)]
+        layers = []
+        layers.append(ConvBNRelu(3, config.discriminator_channels))
         for _ in range(config.discriminator_blocks-1):
             layers.append(ConvBNRelu(config.discriminator_channels, config.discriminator_channels))
 
@@ -22,26 +23,27 @@ class Discriminator(nn.Module):
         self.linear = nn.Linear(config.discriminator_channels, 1)
 
     def forward(self, image):
-        sharpened = None
-        combined = []
-        if image.shape[1]==3:
-            for i in range(image.shape[0]):
-                T1= torch.narrow(image, 0,0 ,1)
-                if T1.shape[0] == 1:
-                    toTensor = transforms.ToTensor()
-                    sqT1 = torch.squeeze(T1,dim=0)
-                    transT1 = torch.reshape(sqT1,(128,128,3))
-                    print(transT1.requires_grad)
-                    if transT1.requires_grad == True:
-                        sharpened = Gabor(transT1.detach().cpu().numpy())
-                    else:
-                        sharpened = Gabor(transT1.cpu())
-                    combined.append(toTensor(sharpened.finalimg).reshape(3,128,128).to("cuda:0"))
-            combined = torch.stack(combined)
-            X = self.before_linear(combined)
-        else:
-            X = self.before_linear(image)
-        # X = self.before_linear(image)
+        # an attmept of utilizing inverse gabor but did not work well to converge at all
+        # sharpened = None
+        # combined = []
+        # if image.shape[1]==3:
+        #     for i in range(image.shape[0]):
+        #         T1= torch.narrow(image, 0,0 ,1).to("cuda:0")
+        #         if T1.shape[0] == 1:
+        #             toTensor = transforms.ToTensor()
+        #             sqT1 = torch.squeeze(T1,dim=0)
+        #             transT1 = torch.reshape(sqT1,(128,128,3))
+        #             # if transT1.requires_grad == True:
+        #             #     sharpened = Gabor(transT1.detach().cpu().numpy())
+        #             # else:
+        #             #     sharpened = Gabor(transT1.cpu())
+        #             sharpened = Gabor(transT1)
+        #             combined.append(toTensor(sharpened.finalimg).reshape(3,128,128).to("cuda:0"))
+        #     combined = torch.stack(combined)
+        #     X = self.before_linear(combined)
+        # else:
+        #     X = self.before_linear(image)
+        X = self.before_linear(image)
         # the output is of shape b x c x 1 x 1, and we want to squeeze out the last two dummy dimensions and make
         # the tensor of shape b x c. If we just call squeeze_() it will also squeeze the batch dimension when b=1.
         X.squeeze_(3).squeeze_(2)
--- model/gabor.py
+++ model/gabor.py
@@ -15,39 +15,56 @@ class Gabor:
 
     finalimg = None
         
-    M = [[a00, 1 - a00 - a02, a02],
+    M = torch.Tensor([[a00, 1 - a00 - a02, a02],
         [a10, 1 - a10 - a12, a12],
-        [a20, a21, 1 - a20 - a21]]
+        [a20, a21, 1 - a20 - a21]]).to("cuda:0")
 
     #Minv = [[2542.56835937500000, -2326.53173828125000, 44.17734527587891],
     #        [-698.04852294921875, 1012.71777343750000, -76.22974395751953],
     #        [-827.36480712890625, 709.83502197265625, 451.38488769531250]]
 
-    Minv = [[11.031566901960783, -9.866943921568629, -0.16462299647058826],
+    Minv = torch.Tensor([[11.031566901960783, -9.866943921568629, -0.16462299647058826],
             [-3.254147380392157,  4.418770392156863,  -0.16462299647058826],
-            [-3.6588512862745097, 2.7129230470588235, 1.9459282392156863]]
+            [-3.6588512862745097, 2.7129230470588235, 1.9459282392156863]]).to("cuda:0")
     def __init__(self,img):
         self.finalimg = self.inverse_gabor(img)
     def toXYB(self,img):
-        rgb = np.asarray(img)
-        # print(rgb)
-        xyb = np.zeros((rgb.shape[0], rgb.shape[1], 3))
+        # rgb = np.asarray(img)
+        rgb = torch.Tensor(img).to("cuda:0")
+        # xyb = np.zeros((rgb.shape[0], rgb.shape[1], 3))
+        xyb = torch.zeros((rgb.shape[0], rgb.shape[1], 3)).to("cuda:0")
         
         bias = 0.0037930732552754493
-        eps = np.array([bias, bias, bias]).reshape(-1, 1)
+        # eps = np.array([bias, bias, bias]).reshape(-1, 1)
+        eps = torch.Tensor([bias, bias, bias]).reshape(-1, 1).to("cuda:0")
+        xybfilt = torch.Tensor([[0.5,-0.5,0],[0.5,0.5,0],[0,0,1]]).to("cuda:0")
+        gamma = None
+        xyblist = []
+        innnerxyblist = []
+        innnerxyb = None
         for r in range(rgb.shape[0]):
             for c in range(rgb.shape[1]):
                 pixel = rgb[r, c].reshape(-1, 1)
-                mix = np.matmul(self.M, pixel) + eps
-                gamma = np.cbrt(mix) + eps
-                xyb[r, c, 0] = (gamma[0] - gamma[1]) / 2
-                xyb[r, c, 1] = (gamma[0] + gamma[1]) / 2
-                xyb[r, c, 2] = gamma[2]
-                
+
+                # mix = np.matmul(self.M, pixel) + eps
+                mix = torch.matmul(self.M, pixel) + eps
+                # gamma = np.cbrt(mix) + eps
+                gamma = torch.pow(mix,1/3) + eps
+                # xyb[r, c, 0] = (gamma[0] - gamma[1]) / 2
+                # xyb[r, c, 1] = (gamma[0] + gamma[1]) / 2
+                # xyb[r, c, 2] = gamma[2]
+                a = torch.matmul(xybfilt,gamma)#.reshape(1,3)
+                innnerxyblist.append(torch.squeeze(a,dim=1))
+            innnerxyb = torch.stack(innnerxyblist)
+            xyblist.append(innnerxyb)
+            innnerxyblist=[]
+        xyb = torch.stack(xyblist)
         return xyb
     def toRGB(self,img):
-        bias = np.array([-0.00100549, -0.00100549, -0.00094081]).reshape(-1, 1)
-        rgb = np.zeros((img.shape[0], img.shape[1], 3))
+        # bias = np.array([-0.00100549, -0.00100549, -0.00094081]).reshape(-1, 1)
+        bias = torch.Tensor([-0.00100549, -0.00100549, -0.00094081]).reshape(-1, 1).to("cuda:0")
+        # rgb = np.zeros((img.shape[0], img.shape[1], 3))
+        rgb = torch.zeros((img.shape[0], img.shape[1], 3)).to("cuda:0")
         
         for r in range(img.shape[0]):
             for c in range(img.shape[1]):
@@ -56,8 +73,10 @@ class Gabor:
                 g_gamma = xyb[1] - xyb[0]
                 b_gamma = xyb[2]
                 
-                mix = np.array([r_gamma ** 3, g_gamma ** 3, b_gamma ** 3]).reshape(-1, 1) + bias
-                pixel = np.matmul(self.Minv, mix).reshape(1, -1)[0]
+                # mix = np.array([r_gamma ** 3, g_gamma ** 3, b_gamma ** 3]).reshape(-1, 1) + bias
+                mix = torch.Tensor([r_gamma ** 3, g_gamma ** 3, b_gamma ** 3]).reshape(-1, 1).to("cuda:0") + bias
+                # pixel = np.matmul(self.Minv, mix).reshape(1, -1)[0]
+                pixel = torch.matmul(self.Minv, mix).reshape(1, -1)[0]
                 
                 rgb[r, c, 0] = pixel[0]
                 rgb[r, c, 1] = pixel[1]
@@ -65,7 +84,9 @@ class Gabor:
                 
         return rgb
     def inverse_gabor(self,img):
-        rgb = np.asarray(img)
+        #rgb = np.asarray(img)
+        norm = transforms.Normalize([-1,-1,-1],[2,2,2])
+        img = norm(img)
         img_xyb = self.toXYB(img)
         # print(img_xyb)
         
@@ -82,27 +103,48 @@ class Gabor:
                 [L, d, r, d, L],
                 [D, L, R, L, D]]
 
-        kernel = np.array(weights)
-        kernel *= -1 / (1 - 2 * (4 * c + 4 * r + 4 * d + 4 * R + 4 * D + 8 * L))
+        # kernel = np.array(weights)
+        # kernel *= -1 / (1 - 2 * (4 * c + 4 * r + 4 * d + 4 * R + 4 * D + 8 * L))
+        kernel1 = torch.Tensor(weights).to("cuda:0")
+        kernel = torch.mul((-1 / (1 - 2 * (4 * c + 4 * r + 4 * d + 4 * R + 4 * D + 8 * L))),kernel1)
         
-        x = signal.convolve2d(img_xyb[:, :, 0], kernel, boundary='symm', mode='same')
-        y = signal.convolve2d(img_xyb[:, :, 1], kernel, boundary='symm', mode='same')
-        b = signal.convolve2d(img_xyb[:, :, 2], kernel, boundary='symm', mode='same')
+        x = signal.convolve2d(img_xyb[:, :, 0].detach().cpu().numpy(), kernel.cpu().numpy()[::-1, ::-1], boundary='symm', mode='same')
+        y = signal.convolve2d(img_xyb[:, :, 1].detach().cpu().numpy(), kernel.cpu().numpy()[::-1, ::-1], boundary='symm', mode='same')
+        b = signal.convolve2d(img_xyb[:, :, 2].detach().cpu().numpy(), kernel.cpu().numpy()[::-1, ::-1], boundary='symm', mode='same')
         
-        result = np.zeros((img_xyb.shape[0], img_xyb.shape[1], 3))
+        # result = np.zeros((img_xyb.shape[0], img_xyb.shape[1], 3))
+        result = torch.ones((img_xyb.shape[0], img_xyb.shape[1], 3)).to("cuda:0")
+        xyblist = []
+        xyblist.append(torch.Tensor(x).to("cuda:0"))
+        xyblist.append(torch.Tensor(y).to("cuda:0"))
+        xyblist.append(torch.Tensor(b).to("cuda:0"))
+        xybtensor = torch.stack(xyblist).reshape(128,128,3)
+        # for r in range(img_xyb.shape[0]):
+        #     for c in range(img_xyb.shape[1]):
+        #         result[r, c, 0] = x[r, c]
+        #         result[r, c, 1] = y[r, c]
+        #         result[r, c, 2] = b[r, c]
+        result = torch.mul(result,xybtensor)
+        # restored = abs(self.toRGB(result))
+        restored = torch.abs(self.toRGB(result))
         
-        for r in range(img_xyb.shape[0]):
-            for c in range(img_xyb.shape[1]):
-                result[r, c, 0] = x[r, c]
-                result[r, c, 1] = y[r, c]
-                result[r, c, 2] = b[r, c]
-                
-        restored = abs(self.toRGB(result))
-        
-        for i in range(3):
-            restored[:, :, i] = restored[:, :, i] / (restored[:, :, i].max() / 255.0)
-            
-        out_img = Image.fromarray(np.uint8(restored)).convert('RGB')
+        # for i in range(3):
+        #     restored[:, :, i] = restored[:, :, i] / (restored[:, :, i].max() / 255.0)
+        rmax = 1/(torch.max(restored[:, :, 0]).item()/255.0)
+        gmax = 1/(torch.max(restored[:, :, 1]).item()/255.0)
+        bmax = 1/(torch.max(restored[:, :, 2]).item()/255.0)
+        rten=torch.Tensor(np.ones((restored.shape[0],restored.shape[0]))*rmax).to("cuda:0")
+        gten=torch.Tensor(np.ones((restored.shape[0],restored.shape[0]))*gmax).to("cuda:0")
+        bten=torch.Tensor(np.ones((restored.shape[0],restored.shape[0]))*bmax).to("cuda:0")
+        converlist = []
+        converlist.append(rten)
+        converlist.append(gten)
+        converlist.append(bten)
+        converten = torch.stack(converlist).reshape(128,128,3)
+        restored = torch.mul(converten,restored)
+        norm1 = transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])
+        restored = norm1(restored)
+        out_img = Image.fromarray(np.uint8(restored.detach().cpu())).convert('RGB')
         return out_img
 # #raw = Image.open('000000000025.jpg')
 # #raw = Image.open('000000000036.jpg')
